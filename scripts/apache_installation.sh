c82e2b191b3fc149d0971c4128315f05e77215a8
anilkumar-vangipuram <anil.vangipuram8292@gmail.com>
2022-03-02 13:03:22 +0530

    adding files first time

---
+++ scripts/apache_installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# install apache
+yum install apache
+#enable the apache
+systemctl enable apache
+#start the apache
+systemctl start apache
+#check the status
+systemctl status apache
+#check the process
+ps -ef | grep -i httpd
